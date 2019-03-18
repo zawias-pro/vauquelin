7c85642934b8934ad2e790a2b446394a427843ea
zawias <zawias@zawias.pro>
2019-03-18 18:10:23 +0100

    textareas

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 const App = () => (
   <div>
-    vauquelin
+    <div>
+    <h1>vauquelin - JSON translator</h1>
+    </div>
+    <div style={{ display: 'flex' }}>
+      <div style={{ width: '50%' }}>
+        <div>input</div>
+        <textarea rows={50} style={{ width: '80%' }} />
+      </div>
+      <div style={{ width: '50%' }}>
+        <div>output</div>
+        <textarea rows={50} style={{ width: '80%' }} value="" onChange={() => {}} />
+      </div>
+    </div>
   </div>
 )
 
