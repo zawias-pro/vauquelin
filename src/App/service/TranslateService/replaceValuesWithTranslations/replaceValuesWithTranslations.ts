12b6f9e917aea2f6e507453189da463c3c7925a3
zawias <zawias@zawias.pro>
2019-03-29 17:11:43 +0100

    reviver used

--- src/App/service/TranslateService/replaceValuesWithTranslations/replaceValuesWithTranslations.ts
+++ src/App/service/TranslateService/replaceValuesWithTranslations/replaceValuesWithTranslations.ts
@@ -1,5 +1,15 @@
+import _isString from 'lodash/isString'
+
 import { ITranslationObject } from '../../../interfaces/ITranslationObject'
 
 export const replaceValuesWithTranslations = (inputJSON: string, translations: ITranslationObject[]) => {
-  return inputJSON
+  const modified = JSON.parse(inputJSON, (key, value) => {
+    if (_isString(value)) {
+      return 'translated'
+    }
+
+    return value
+  })
+
+  return JSON.stringify(modified)
 }
