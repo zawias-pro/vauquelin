e3b5ad31c663ceea9ea244c41bc8c9ea7195f1bc
Paweł Zawiślak <zawias@zawias.pro>
2019-05-09 20:02:23 +0200

    yandex license (#1)

--- src/App/components/OutputSection/OutputSection.tsx
+++ src/App/components/OutputSection/OutputSection.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react'
+import Link from '@material-ui/core/Link'
 import Paper from '@material-ui/core/Paper'
 import { useTranslation } from 'react-i18next'
 import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import { Provider } from '../../../configuration/providers/Provider'
 
 interface OutputSectionProps {
   value: string
+  provider: Provider
 }
 
 const OutputSection: React.FunctionComponent<OutputSectionProps> = ({
   value,
+  provider,
 }) => {
   const { t } = useTranslation()
 
@@ -24,6 +29,17 @@ const OutputSection: React.FunctionComponent<OutputSectionProps> = ({
         value={value}
         disabled={!value}
       />
+      {provider.id === 'yandex' && value && (
+        <Typography variant="body1">
+          Powered by Yandex.Translate:&nbsp;
+          <Link
+            href="http://translate.yandex.com/"
+            target="_blank"
+          >
+            translate.yandex.com
+          </Link>
+        </Typography>
+      )}
     </Paper>
   )
 }
